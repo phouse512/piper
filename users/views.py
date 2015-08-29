730e2b13e8a88c3201104b760288f6c9d1ae1e49
phouse512 <philiphouse2015@u.northwestern.edu>
2015-08-29 13:32:26 -0500

    setting up basic user urls

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# Create your views here.
+import json
+
+from django.http import HttpResponse
+from users.models import User
+
+def index(request):
+    users = User.objects.get().all()
+    return HttpResponse(json.dumps(users), content_type="application/json")
