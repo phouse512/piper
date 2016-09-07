3b91c3d5b218ba04e2f1a312d6a3d5c4f4f8577f
phouse512 <philiphouse2015@u.northwestern.edu>
2016-09-07 15:50:06 -0500

    cleaning up

--- arkaios/views.py
+++ arkaios/views.py
@@ -6,20 +6,21 @@ from django.shortcuts import render
 from models import Attendee
 # Create your views here.
 
+
 def tracking(request, group_hash):
-    context= { 'url_root': request.get_host(), 'group_hash': group_hash }
+    context = {'url_root': request.get_host(), 'group_hash': group_hash}
     return render(request, 'largegroup.html', context)
 
 
 def search(request, group_hash):
-    search_dict = { 'group_hash': group_hash}
+    search_dict = {'group_hash': group_hash}
     for key in request.GET:
         if request.GET.get(key, ''):
             updated_key = key + '__icontains'
             search_dict[updated_key] = request.GET.get(key)
 
     returned_attendees = Attendee.objects.filter(**search_dict)
-    results_dict = { "results": [] }
+    results_dict = {"results": []}
     for attendee in returned_attendees:
         temp_dict = {
             "year": attendee.year,
@@ -29,8 +30,4 @@ def search(request, group_hash):
         }
         results_dict['results'].append(temp_dict)
 
-
-
-
     return HttpResponse(json.dumps(results_dict))
-
