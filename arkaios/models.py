30f370148e9d3a9d9d5ace08e26505982fb0cd6c
phouse512 <philiphouse2015@u.northwestern.edu>
2016-09-18 16:21:58 +0800

    add admin user

--- arkaios/models.py
+++ arkaios/models.py
@@ -55,6 +55,14 @@ class Group(models.Model):
     name = models.CharField(max_length=40)
 
 
+class GroupAdmin(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    password = models.CharField(max_length=50)
+    group = models.ForeignKey(Group)
+
+
 class GroupGrades(models.Model):
 
     id = models.AutoField(primary_key=True)
