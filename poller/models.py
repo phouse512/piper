e4fa80466bbf9dfacc96531417a5fb59f2d469cd
phouse512 <philiphouse2015@u.northwestern.edu>
2016-10-23 16:30:08 +0800

    adding new polls

---
+++ poller/models.py
@@ -0,0 +1,45 @@
+import datetime
+
+from django.db import models
+
+# Create your models here.
+
+class Users(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=150)
+    pin = models.CharField(max_length=10)
+    email = models.CharField(max_length=150)
+    join_date = models.DateTimeField(default=datetime.datetime.now)
+
+
+class Scores(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    score = models.IntegerField()
+    created = models.DateTimeField(default=datetime.datetime.now)
+    user = models.ForeignKey(Users)
+
+
+class Poll(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    open = models.BooleanField(default=False)
+    finished = models.BooleanField(default=False)
+    weight = models.IntegerField(default=1)
+    question = models.CharField(max_length=250)
+    correct_id = models.IntegerField(default=None)
+    created = models.DateTimeField(default=datetime.datetime.now)
+    finish_time = models.DateTimeField()
+
+
+class Answers(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    text = models.CharField(max_length=200)
+    poll = models.ForeignKey(Poll)
+
+
+
+
+
