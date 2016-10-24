6c69a08245d7862282bd69e4e3652b7557f3b87b
phouse512 <philiphouse2015@u.northwestern.edu>
2016-10-25 00:56:53 +0800

    here we go

--- poller/management/commands/seed_data.py
+++ poller/management/commands/seed_data.py
@@ -15,27 +15,82 @@ class Command(BaseCommand):
         #     email='philiphouse2015@u.northwestern.edu'
         # )
 
-        poll = Poll.objects.filter(id=4).first()
-
-        print poll.votes.all()
-
-        # new_poll = Poll.objects.create(
-        #     open=True,
-        #     finished=False,
-        #     weight=1,
-        #     question='Who will win Game 1 of the World Series?',
-        #     correct_id=0,
-        #     finish_time=datetime.datetime.now() + datetime.timedelta(days=3)
-        # )
+        # poll = Poll.objects.filter(id=4).first()
         #
-        # cleveland_answer = Answers.objects.create(
-        #     text="Cleveland Indians",
-        #     poll=new_poll
-        # )
+        # print poll.votes.all()
+
+        new_poll = Poll.objects.create(
+            open=True,
+            finished=False,
+            weight=1,
+            question='Who will win Game 1 of the World Series?',
+            correct_id=0,
+            finish_time="2016-10-26 00:00:00+00"
+        )
         #
-        # chicago_answer = Answers.objects.create(
-        #     text="Chicago Cubs",
-        #     poll=new_poll
-        # )
+        cleveland_answer = Answers.objects.create(
+            text="Cleveland Indians",
+            poll=new_poll
+        )
 
+        chicago_answer = Answers.objects.create(
+            text="Chicago Cubs",
+            poll=new_poll
+        )
 
+        nyPoll = Poll.objects.create(
+            open=True,
+            finished=False,
+            weight=1,
+            question='New York Knicks versus Cleveland Cavaliers, NBA Opener',
+            correct_id=0,
+            finish_time="2016-10-25 23:30:00+00"
+        )
+
+        cavs = Answers.objects.create(
+            text="Cleveland Cavaliers",
+            poll=nyPoll
+        )
+
+        knicks = Answers.objects.create(
+            text="New York Knicks",
+            poll=nyPoll
+        )
+
+        utahPoll = Poll.objects.create(
+            open=True,
+            finished=False,
+            weight=1,
+            question='Utah Jazz versus Portland Trailblazers, NBA Opener',
+            correct_id=0,
+            finish_time="2016-10-26 02:00:00+00"
+        )
+
+        utah = Answers.objects.create(
+            text="Utah Jazz",
+            poll=utahPoll
+        )
+
+        portland = Answers.objects.create(
+            text="Portland Trailblazers",
+            poll=utahPoll
+        )
+
+        gsPoll = Poll.objects.create(
+            open=True,
+            finished=False,
+            weight=1,
+            question='San Antonio Spurs versus Golden State Warriors, NBA Opener',
+            correct_id=0,
+            finish_time="2016-10-26 02:30:00+00"
+        )
+
+        spurs = Answers.objects.create(
+            text="San Antonio Spurs",
+            poll=gsPoll
+        )
+
+        gs = Answers.objects.create(
+            text="Golden State Warriors",
+            poll=gsPoll
+        )
