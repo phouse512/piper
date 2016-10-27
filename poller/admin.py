15b631720e384367955a66ce8de2dbc1bd8a3585
phouse512 <philiphouse2015@u.northwestern.edu>
2016-10-27 21:27:57 +0800

    adding admin

--- poller/admin.py
+++ poller/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from poller.models import Poll
+from poller.models import Answers
+from poller.models import Votes
+from poller.models import Scores
+from poller.models import Users
 
 # Register your models here.
+
+admin.site.register(Poll)
+admin.site.register(Answers)
+admin.site.register(Votes)
+admin.site.register(Scores)
+admin.site.register(Users)
