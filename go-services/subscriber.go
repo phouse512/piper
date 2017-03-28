658f1f0c81a2e1aaf5a0e21b4337cd6d3fc7b08b
phouse512 <philiphouse2015@u.northwestern.edu>
2017-03-28 00:00:42 -0700

    adding gmail subscriber

---
+++ go-services/subscriber.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"cloud.google.com/go/pubsub"
+	"encoding/json"
+	"fmt"
+	"golang.org/x/net/context"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+)
+
+var (
+	topic *pubsub.Topic
+
+	messagesMu sync.Mutex
+	messages   []string
+)
+
+const maxMessages = 10
+
+func main() {
+	fmt.Println("vim-go")
+
+	ctx := context.Background()
+
+	client, err := pubsub.NewClient(ctx, mustGetenv("GCLOUD_PROJECT"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	topic, _ = client.CreateTopic(ctx, mustGetenv("PUBSUB_TOPIC"))
+
+	http.HandleFunc("/pubsub/push", pushHandler)
+	http.ListenAndServe(":8081", nil)
+}
+
+func mustGetenv(k string) string {
+	v := os.Getenv(k)
+	if v == "" {
+		log.Fatalf("%s environment variable not set.", k)
+	}
+
+	return v
+}
+
+type pushRequest struct {
+	Message struct {
+		Attributes map[string]string
+		Data       []byte
+		ID         string `json:"message_id"`
+	}
+}
+
+func pushHandler(w http.ResponseWriter, r *http.Request) {
+	msg := &pushRequest{}
+	if err := json.NewDecoder(r.Body).Decode(msg); err != nil {
+		http.Error(w, fmt.Sprintf("Could not decode body: %v", err), http.StatusBadRequest)
+		return
+	}
+
+	messagesMu.Lock()
+	defer messagesMu.Unlock()
+
+	messages = append(messages, string(msg.Message.Data))
+}
