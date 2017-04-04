be498a613ce144b8807890df1639e09e568c2d25
phouse512 <philiphouse2015@u.northwestern.edu>
2017-04-04 14:48:51 -0700

    adding upload to s3 from handler

--- lambda/receiptReceiver/handler.py
+++ lambda/receiptReceiver/handler.py
@@ -1,8 +1,20 @@
+import base64
+import boto3
+
+s3 = boto3.resource('s3')
+
+
 def lambda_handler(event, context):
     # TODO implement
-    
-    print(event)
-    print(context)
+
+    response = s3.Bucket('receipt-storage').put_object(
+        Body=event['body'],
+        ContentEncoding='base64',
+        ContentType='application/pdf',
+        Key='test_obj.pdf'
+    )
+
+    print(response)
     
     return {
         'statusCode': 200,
